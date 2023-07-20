02551820f8fadaa2e63e140643ab1c146d71c332
CindyZhannnn <138415673+CindyZhannnn@users.noreply.github.com>
2023-07-19 22:41:09 -0700

    1111

--- Assets/Script/Level1 Script/Replicate.cs
+++ Assets/Script/Level1 Script/Replicate.cs
@@ -7,11 +7,9 @@ public class Replicate : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject playerPrefab;
-    public GameObject playerSplitPrefab;
     //public Animator walking;
     public Enemy en;
-    public PlayerMove pm;
-    public Gamem gm;
+    public GameMana gm;
     public Animator split;
     
     void Start()
@@ -41,31 +39,37 @@ public class Replicate : MonoBehaviour
         playerSpriteRenderer.enabled = false;
 
         child.gameObject.SetActive(true);
-        //playerObject.transform.localScale = playerObject.transform.localScale * 0.8f;
-        //child.transform.localScale = child.transform.localScale * 1.25f;
         print("Replicate");
-        //split.SetBool("IsSplitting", true);
         yield return new WaitForSeconds(1f);
 
-        //split.SetBool("IsSplitting", false);
 
-       
         child.gameObject.SetActive(false);
         playerSpriteRenderer.enabled = true;
 
-
+        // Calculate the position for the new player object
         Vector3 currentPosition = playerObject.transform.position;
         currentPosition.y += -0.07f;
+
+        // Calculate the scale for the new player object
         Vector3 currentScale = playerObject.transform.localScale;
+        Vector3 smallerScale = currentScale * 0.8f;
+
+        // Get the width of the playerObject sprite
+        float playerObjectWidth = playerSpriteRenderer.bounds.size.x;
+
+        // Get the width of the player1 object sprite
+        float player1Width = playerPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+
+        // Calculate the distance to keep between the objects
+        float distance = (playerObjectWidth + player1Width)*1.0f;
+
+        // Instantiate the new player object next to the playerObject
+        GameObject player1 = Instantiate(playerPrefab, currentPosition + new Vector3(distance, 0f, 0f), Quaternion.identity);
+
+        // Set the scale of the new player object
+        player1.transform.localScale = smallerScale;
 
 
-        Vector3 smallerScale = currentScale *0.8f;
-
-        float offset = 1.5f;
-
-        GameObject player1 = Instantiate(playerPrefab, currentPosition + new Vector3(offset, 0f, 0f), Quaternion.identity);
-        
-       
         player1.tag = "PlayerSub";
         gm.playerGameObjects.Add(player1);
         print("Replicate tag: " + player1.tag);
---
+++ Assets/Script/Tutorial Script/GameMana.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMana : MonoBehaviour
+{
+    public Dictionary<GameObject, bool> players = new Dictionary<GameObject, bool>();
+    public ArrayList playerGameObjects = new ArrayList();
+    public int countNum;
+    public GameObject SplitAni;
+    public Enemy en;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject playerObject in playerObjects)
+        {
+            if (!players.ContainsKey(playerObject))
+            {
+                playerGameObjects.Add(playerObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(players.Count);
+        if (Input.GetKeyDown("p"))
+        {
+            SwitchBetweenPlayer();
+        }
+    }
+
+    public void SwitchBetweenPlayer()
+    {
+        if (countNum >= playerGameObjects.Count)
+        {
+            countNum = countNum - playerGameObjects.Count;
+        }
+        print("current count: " + countNum);
+        if (countNum == 0)
+        {
+            GameObject previousPlayerObject = (GameObject)playerGameObjects[playerGameObjects.Count - 1];
+            previousPlayerObject.tag = "PlayerSub";
+            PlayerMove previousScript = previousPlayerObject.GetComponent<PlayerMove>();
+            previousScript.GotoSleep();
+            previousScript.enabled = false;
+
+        }
+        else
+        {
+            GameObject previousPlayerObject = (GameObject)playerGameObjects[countNum - 1];
+            PlayerMove previousScript = previousPlayerObject.GetComponent<PlayerMove>();
+            previousPlayerObject.tag = "PlayerSub";
+            previousScript.GotoSleep();
+            previousScript.enabled = false;
+        }
+
+        GameObject currentPlayerObject = (GameObject)playerGameObjects[countNum];
+        PlayerMove currentScript = currentPlayerObject.GetComponent<PlayerMove>();
+        currentPlayerObject.tag = "Player";
+        //en.findPlayerTag();
+
+        if (currentScript != null)
+        {
+            currentScript.enabled = true;
+            currentScript.WakeUp();
+        }
+
+        countNum++;
+
+        print("num of obj: " + playerGameObjects.Count);
+
+
+        //print(players[currentPlayerObject]);
+    }
+
+    public void haveNewObj(GameObject gameobj)
+    {
+        PlayerMove previousScript = gameobj.GetComponent<PlayerMove>();
+        previousScript.GotoSleep();
+        previousScript.enabled = false;
+    }
+
+    public void PlaySplitAni()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        Transform playerTransform = playerObject.transform;
+        Transform splitAni = playerTransform.GetChild(0);
+        splitAni.gameObject.SetActive(true);
+
+    }
+}
