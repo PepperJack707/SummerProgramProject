a3c5210fb92508816a8ea6afd455c46dbf7142a6
CindyZhannnn <138415673+CindyZhannnn@users.noreply.github.com>
2023-07-20 18:59:20 -0700

    111

---
+++ Assets/Script/PlayerSize.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSize : MonoBehaviour
+{
+    
+        public float currentSize = 1f; // Size of the player character
+
+        // Additional code for updating the player size based on some game mechanics
+        // You can adjust this logic as per your game requirements.
+    }
+
+
+
